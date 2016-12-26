47c736b00abca9b19d4e3494aae9930e9ec46d5d
djneo33 <djneo33@yahoo.com>
2016-12-26 10:19:50 -0800

    Update

--- app/src/main/java/com/example/omega/inventoryConsolidated/Insert.java
+++ app/src/main/java/com/example/omega/inventoryConsolidated/Insert.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -33,48 +34,16 @@ public class Insert extends AppCompatActivity {
         Bundle extra = getIntent().getExtras();
         paper = extra.getString("paper");
         newPaper = extra.getBoolean("createPaper");
-        Button button = (Button) findViewById(R.id.button);
-        button.setText("Create new insert for " + paper);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(Insert.this);
-
-
-// Set up the input
-                final EditText input = new EditText(Insert.this);
-// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-                input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_TEXT);
-                builder.setView(input);
-
-// Set up the buttons
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        m_Text = input.getText().toString();
-                        Intent intent = new Intent(Insert.this, date.class);
-                       intent.putExtra("insert",m_Text);
-                        intent.putExtra("paper",paper);
-                        startActivity(intent);
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
 
 
-                    }
-                });
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
 
-                builder.show();
 
-            }
-        });
 
-        listview = (ListView) findViewById(R.id.listView2);
+
+        listview = (ListView) findViewById(R.id.listViewInsert);
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -84,6 +53,7 @@ public class Insert extends AppCompatActivity {
                 intent.putExtra("insert",clicked);
                 intent.putExtra("paper",paper);
                 intent.putExtra("createPaper",newPaper);
+
                 startActivity(intent);
             }
         });
@@ -100,7 +70,7 @@ public class Insert extends AppCompatActivity {
 
 
             try {
-                String query = "select distinct `insert` from `Inventory`."+"`"+paper+"`"+ "union select distinct `insert` from `InventoryBackLog`."+"`"+paper+"`";
+                String query = "select * from `Inventory`.`Insert`";
 
 
 
